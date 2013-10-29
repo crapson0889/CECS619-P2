bc2d35e4bf57495136556ddaeab7bebc45a3eb02
Phillip Rose <omnomcake3549@gmail.com>
2013-10-29 15:15:50 -0400

    Update
    + New function in main to run experiment on all three trees with the
    same numbers, to make the results more accurate (Commented out the calls
    in main until the modified code is done)
    
    #The binExperiment and digExperiment functions will be redundant and can
    be removed once this is finished.
    
    + modDigitalTree.java : I don't think this class actually needs any
    work, it may not even be necessary actually, since this is basically
    just a digital tree with a different input, so we could probably just
    make a second digital tree with the correct value inserted.

--- Project2/src/project2/main.java
+++ Project2/src/project2/main.java
@@ -38,12 +38,25 @@ public class main {
             int expNum = 100 + (200 * expCount);
             float[] binResults = BinExperiment(expNum, binTree);
             float[] digResults = DigExperiment(expNum, digTree);
+            
+//            float[] results = experiment(expNum, binTree, digTree, modDigTree);
+//            
+//            System.out.println(expNum + " Keys");
+//            System.out.println("--------");
+//            System.out.println("Binary Tree Results: ");
+//            System.out.println("Minimum = " + results[0] + " Maximum = " + results[1] + " Average = " + results[2]);
+//            System.out.println("Digital Tree Results: ");
+//            System.out.println("Minimum = " + results[3] + " Maximum = " + results[4] + " Average = " + results[5]);
+//            System.out.println("Digital Tree Results: ");
+//            System.out.println("Minimum = " + results[6] + " Maximum = " + results[7] + " Average = " + results[8]);
+
             System.out.println(expNum + " Keys");
             System.out.println("--------");
             System.out.println("Binary Tree Results: ");
             System.out.println("Minimum = " + binResults[0] + " Maximum = " + binResults[1] + " Average = " + binResults[2]);
             System.out.println("Digital Tree Results: ");
             System.out.println("Minimum = " + digResults[0] + " Maximum = " + digResults[1] + " Average = " + digResults[2]);
+
             System.out.println("");
             expCount++;
         }
@@ -52,7 +65,96 @@ public class main {
   
                 
     }
-     
+    public static float[] experiment(int numKeys, binaryTree binTree, digitalTree digTree, modDigitalTree modTree)
+    {
+        float[] results = new float[9];
+        List<Integer> binCounts = new ArrayList<>();
+        List<Integer> digCounts = new ArrayList<>();
+        List<Integer> modCounts = new ArrayList<>();
+        int[] min = new int[3];
+        int[] max = new int[3];
+        int[] total = new int[3];
+            
+        int i = 0;
+        while(i < numKeys)
+        {
+            int random = (int)(Math.random() * 65536 + 1);
+            if(binTree.lookup(random))
+            {
+                digTree.lookup(random);
+                modTree.lookup(random);
+                binCounts.add(binTree.count);
+                digCounts.add(digTree.count);
+                modCounts.add(modTree.count);
+                i++;
+            }
+            
+            for(int j = 0; j < numKeys; j++)
+            {
+                if(j == 0)
+                {
+                    min[0] = binCounts.get(j); // 0 = binary
+                    min[1] = digCounts.get(j); // 1 = digital
+                    min[2] = modCounts.get(j); // 2 = modified
+                    
+                    max[0] = binCounts.get(j);
+                    max[1] = digCounts.get(j);
+                    max[2] = modCounts.get(j);
+                }
+
+                if(binCounts.get(j) < min[0])
+                {
+                    min[0] = binCounts.get(j);
+                }
+                
+                if(digCounts.get(j) < min[1])
+                {
+                    min[1] = digCounts.get(j);
+                }
+                
+                if(modCounts.get(j) < min[2])
+                {
+                    min[2] = modCounts.get(j);
+                }
+                
+                if(binCounts.get(j) > max[0])
+                {
+                    max[0] = binCounts.get(j);
+                }
+                
+                if(digCounts.get(j) > max[1])
+                {
+                    max[1] = digCounts.get(j);
+                }
+                
+                if(modCounts.get(j) > max[2])
+                {
+                    max[2] = modCounts.get(j);
+                }
+         
+                total[0] = total[0] + binCounts.get(j);
+                total[1] = total[1] + digCounts.get(j);
+                total[2] = total[2] + modCounts.get(j);
+                
+            }
+  
+        }
+        //Results Array: 
+        //0-2 = bin min, max, avg
+        results[0] = min[0];
+        results[1] = max[0];
+        results[2] = total[0]/numKeys;
+        //3-5 = dig min, max, avg
+        results[3] = min[1];
+        results[4] = max[1];
+        results[5] = total[1]/numKeys;
+        //6-8 = mod min, max, avg
+        results[6] = min[2];
+        results[7] = max[2];
+        results[8] = total[2]/numKeys;
+        return results;
+    }
+    
     public static float[] BinExperiment(int numKeys, binaryTree tree)
     {
         List<Integer> counts = new ArrayList<>();
@@ -104,7 +206,58 @@ public class main {
     
     public static float[] DigExperiment(int numKeys, digitalTree tree) throws StringIndexOutOfBoundsException
     {
-        List<Integer> counts = new ArrayList<Integer>();
+        List<Integer> counts = new ArrayList<>();
+        float[] result = new float[4];
+        int min = 0;
+        int max = 0;
+        int total = 0;
+        
+        
+        int i = 0;
+        while(i < numKeys)
+        {
+            int random = (int)(Math.random() * 65536 + 1);
+            if(tree.lookup(random))
+            {
+                counts.add(tree.count);
+                i++;
+                //System.out.println(i);
+            }
+        }
+        
+        for(int j = 0; j < counts.size(); j++)
+        {
+            if(j == 0)
+            {
+                min = counts.get(j);
+                max = counts.get(j);
+            }
+            
+            if(counts.get(j) < min)
+            {
+                min = counts.get(j);
+            }
+            
+            if(counts.get(j) > max)
+            {
+                max = counts.get(j);
+            }
+            
+            total = total + counts.get(j);
+        }
+        
+        result[0] = min;
+        result[1] = max;
+        result[2] = total/counts.size();
+        result[3] = counts.size();
+        
+        return result;
+    }
+    
+ 
+    public static float[] modExperiment(int numKeys, digitalTree tree) throws StringIndexOutOfBoundsException
+    {
+        List<Integer> counts = new ArrayList<>();
         float[] result = new float[4];
         int min = 0;
         int max = 0;
---
+++ Project2/src/project2/modDigitalTree.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+/**
+ *
+ * @author p0ros_000
+ */
+public class modDigitalTree extends digitalTree {
+    
+    public void modDigitalTree()
+    {
+        root = null;
+    }
+    
+    //Insert and Lookup functions should work the same, just looking at less bits
+    
+    //TODO: Function to seperate out the relevant bits and call Instert/Lookup
+    
+}
